2f8898ebf18c88645fc15f18d32863adeb7f751c
AlkoholTesta <peer-henrik@t-online.de>
2025-10-24 15:44:32 +0200

    Update and rename v7.1[pöör].c to v7.2.c

--- code/v7.2.c
+++ code/v7.2.c
@@ -9,7 +9,7 @@
 // The authors are not responsible for any damage caused by the use of this code.
 //This code may be used for educational Purposes
 // Code created by:  Peer Siems, 2025 / Marlon Jost, 2025
-// Soccerversion 7.1 STABLE RELEASE
+// Soccerversion 7.2 STABLE RELEASE
 
 #include "hitechnic-compass.h"
 #include "hitechnic-irseeker-v2.h"
@@ -226,3 +226,4 @@ task main();
 	}
 
 }
+
